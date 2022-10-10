6365990fbda55f297cd8c4d88672aab8f9669a53
aarnaspacecrafter13 <103654092+aarnaspacecrafter13@users.noreply.github.com>
2022-10-10 19:59:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createcanvas(300,300);
+    canvas.center();
+}
+
+function draw() {
+}
+function take_snapshot(){
+    SVGAElement('myFILTERTMAGE.png');
+}
